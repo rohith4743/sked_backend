858c67719ce7154831092f7966066a460cc465f7
rohith4743 <rohithkankipati4743@gmail.com>
2024-12-02 15:17:37 -0500

    somecommit

--- Sked/src/main/java/com/rohithkankipati/projects/Sked/dto/LoginRequestDTO.java
+++ Sked/src/main/java/com/rohithkankipati/projects/Sked/dto/LoginRequestDTO.java
@@ -2,18 +2,14 @@ package com.rohithkankipati.projects.Sked.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 public class LoginRequestDTO {
 	
 	@NotBlank(message = "{username.required}")
-    @Size(min = 3, max = 255, message = "{username.size}")
 	@Pattern(regexp = "^[a-zA-Z0-9._-]{3,}$", message = "{username.pattern}")
     private String username;
 
     @NotBlank(message = "{password.required}")
-    @Size(min = 8, max = 100, message = "{password.size}")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).*$", message = "{password.pattern}")
     private String password;
 
     // Getters and setters
